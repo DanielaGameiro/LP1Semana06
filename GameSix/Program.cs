440e54743de264f6b8018b302202d97fedf57048
Daniela Gameiro <danielacgameiro@hotmail.com>
2023-04-19 13:43:34 +0100

    Solve exercise 5

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,8 +6,32 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Enemy enemy1 = new Enemy("      Loki       ");
-            Console.WriteLine(enemy1.GetName());
+            // Variáveis
+            int numEnemies;
+            Enemy[] enemies;
+
+            // Pedir ao utilizador o número de inimigos
+            Console.Write("Number of enemies: ");
+            numEnemies = int.Parse(Console.ReadLine());
+
+            // Pedir o utilizador o nome para casa inimigo e guardá-lo num array
+            enemies = new Enemy[numEnemies];
+
+            for (int i = 0; i < numEnemies; i++)
+            {
+                string name;
+
+                Console.Write($"Name of enemy #{i}: ");
+                name = Console.ReadLine();
+
+                enemies[i] = new Enemy(name);
+            }
+
+            // Imprimir o(s) nome(s)
+            foreach (Enemy enemy in enemies)
+            {
+                Console.WriteLine($">>>{enemy.GetName()}<<<");
+            }
         }
     }
 }
